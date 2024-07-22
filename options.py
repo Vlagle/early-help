2da8c42966dd094dd8b1ded4cae353c9e7d83b7a
Vladislav Glebov <glebov7vlad@yandex.ru>
2024-07-22 16:36:57 +0500

    Update options.py

--- options.py
+++ options.py
@@ -210,7 +210,6 @@ class optionsMenu(discord.ui.View):
 
     @discord.ui.button(label="Закрыть окно/Close window", emoji="❌", style=discord.ButtonStyle.red)
     async def rename(self, interaction:discord.Interaction, button: discord.ui.button):
-        await interaction.response.send_modal(renameChannelModal())
         author = interaction.user
         embed2 = discord.Embed(description=f'Вы можете нажать на кнопку только один раз! / You can only select a button once! \n\nСообщение будет удалено через 30 секунд. / The message will be deleted after 30 seconds.', color=embedColor)
         embed2.set_author(name=f'{author}', icon_url=f'{author.display_avatar}')
